65b4243edd39c92e41e927ff55eac549e8ca2949
Reddy <247340@NTTDATA.COM>
2022-10-26 16:32:05 +0530

    reduxtoolkit with saga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {getCatsFetch} from './catState';
 
 function App() {
+
+  const cats = useSelector(state => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]);
+  console.log(cats);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>CAT SPEVIES GALLERY</h1>
+     <p>Images of diferenct species of cats. Lots of cats for your viewing pleasure.</p>
+     <hr/>
+     <div className="Gallery">
+      {cats.map(cat =>
+        <div key={cat.id} className="row">
+          <div className="column column-left">
+            <img
+              alt={cat.name}
+              src={cat.image.url}
+              width="200"
+              height="200"
+            />
+            </div>
+          <div className='column column-right'>
+            <h2>{cat.name}</h2>
+            <h5>Temperament: {cat.temperament}</h5>
+            <p>{cat.description}</p>
+          </div>
+        </div>          
+      )}
+     </div>
+     <button>VIEW MORE CATS</button>
     </div>
   );
 }
